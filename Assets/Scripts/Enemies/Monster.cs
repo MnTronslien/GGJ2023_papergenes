52e9db0884977def2363b8412b53fd993b4a0b3c
Mattias T <mntronslien@gmail.com>
2023-02-05 16:05:45 +0100

    ok nå thpmas - fornøyd????

--- Assets/Scripts/Enemies/Monster.cs
+++ Assets/Scripts/Enemies/Monster.cs
@@ -3,15 +3,39 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public Genome genome;
+    public GameObject player;
+
+    public Animator animator;
 
     //Maximum attack didtance is calculated from the longest reach of both arms
-    private float MaxAttackDistance => 6;
+    private float MaxAttackDistance => Mathf.Max(genome.LeftArmGene.length, genome.RightArmGene.length);
 
-    //Start , get player reference
+    
     void Start()
     {
-
+//Get the player
         player = FindAnyObjectByType<Player>().gameObject;
+        //TODO: Spawn Body from geneome
+
+         if(genome == null)
+        {
+            genome = Genome.CreateRandomGenome();
+        }
+
+        Leg l = Instantiate(GlobalInfo.playerGenome.LegsGene, animator.transform, false);
+        Torso t = Instantiate(GlobalInfo.playerGenome.BodyGene, l.torsoPos.position, Quaternion.identity, l.transform);
+        GeneExpression h = Instantiate(GlobalInfo.playerGenome.HeadGene, t.Head.position, Quaternion.identity, t.transform);
+        GeneExpressionFlippable f = Instantiate(GlobalInfo.playerGenome.LeftArmGene, t.FrontArm.position, Quaternion.identity, t.transform);
+        GeneExpressionFlippable b = Instantiate(GlobalInfo.playerGenome.RightArmGene, t.BackArm.position, Quaternion.identity, t.transform);
+
+        f.back.SetActive(false);
+        b.front.SetActive(false);
+
+        l.name = "Legs";
+        t.name = "Torso";
+        h.name = "Head";
+        f.name = "Front";
+        b.name = "Back";
     }
 
 
