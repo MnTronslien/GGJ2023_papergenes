948eed7a3308e011d7a61158eefd43fc0ebe1ac8
Sabotender <admin@sabotender.com>
2023-02-05 15:28:57 +0100

    parts

---
+++ Assets/Scripts/Genetics/BodyParts/Torso.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torso : GeneExpression
+{
+    public Transform Head;
+    public Transform FrontArm;
+    public Transform BackArm;
+}
---
+++ Assets/Scripts/Genetics/GeneExpressionFlippable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneExpressionFlippable : GeneExpression
+{
+    public GameObject front;
+    public GameObject back;
+}
